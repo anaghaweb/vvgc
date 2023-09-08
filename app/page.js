c7827e922a81d58e74f406bfe71bc3a23a6a0b68
anaghaweb <anaghawebdev@gmail.com>
2023-09-08 16:55:48 +0530

    removed playfair font

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { Playfair } from 'next/font/google'
+
 
 // If loading a variable font, you don't need to specify the font weight
-const playfair = Playfair({
-  subsets: ['latin'],
-  variable: '--font-playfair',
-})
+
 
 export const metadata = {
   title: 'Home',
@@ -15,7 +12,7 @@ export const metadata = {
 export default function Home() {
   return (
     <React.Fragment>
-      <section className={`h-[70vh] w-full bg-hero-pattern bg-center bg-no-repeat bg-cover flex justify-center items-center text-6xl text-white ${playfair.variable} font-sans font-[300]`}>
+      <section className={`h-[70vh] w-full bg-hero-pattern bg-center bg-no-repeat bg-cover flex justify-center items-center text-6xl text-white  font-sans font-[300]`}>
         <div className="w-[280px] h-auto"><h1>A Home For Hindu Devotees</h1></div>
       </section>
 
