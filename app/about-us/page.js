0009cbce83ad094b53cfcd1832b1017a4149ab7b
anaghastaff <anaghastaff123@gmail.com>
2024-03-30 22:21:06 +0530

    removed about us data. added placeholder

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -25,11 +25,11 @@ export default function About() {
                        </div>
                     </Hero_Image_Wrapper> */}
 
-                <div className="w-full bg-white">
-                    <section className="block max-w-full relative h-[1000px] p-5 overflow-x-auto overflow-y-hidden">
-
-                        <iframe src={vvgcCommitteeURL} className="w-[900px] h-[1200px] md:mx-auto" ></iframe>
-
+                <div className="w-full bg-white flex flex-col justify-center items-center h-[70vh]">
+                    <section className="block max-w-full relative p-5 overflow-x-auto overflow-y-hidden ">
+                            <div className="text-3xl font-semibold text-center underline text-red-900">Our Mission</div>
+                        {/* <iframe src={vvgcCommitteeURL} className="w-[900px] h-[1200px] md:mx-auto" ></iframe> */}
+                            <div className="p-3 flex justify-center items-center text-center text-2xl">The Mission of Vaidica Vidhya Ganapathi Center (VVGC), a Sanatana Dharma Temple, is to provide a place for worship, educational and cultural services to the bay area community.</div>
                     </section>
                 </div>
 
