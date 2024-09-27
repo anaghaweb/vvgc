aa54390d2a3f5f4edcdfbcb6c1baa283c05178df
anaghastaff <anaghastaff123@gmail.com>
2024-09-28 00:09:51 +0530

    adding twitter/og images

--- app/about-us/opengraph-image.tsx
+++ app/about-us/opengraph-image.tsx
@@ -21,26 +21,20 @@ export default async function Image() {
       // ImageResponse JSX element
      // Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
 
- <div tw='text-xl bg-white w-full h-full flex flex-col justify-center items-center p-2 font-inter font-semibold'>
- 
- <h1 tw='text-2xl font-inter font-bold'>About VVGC</h1>
-        
-        <h3 tw='text-xl font-inter my-1 max-w-[768px] text-center'>
-        The Mission of Vaidica Vidhya Ganapathi Center (VVGC), a Sanatana Dharma Temple, is to provide a place for worship, educational and cultural services to the bay area community.
-        </h3>       
-        
-        <div tw='flex flex-row gap-2 items-center'>
-            <p>
-                Visit us at: 
-            </p>
-            <a href="https://vvgc.org" target='_break' tw="underline text-blue-800"> vvgc.org</a>
-        </div>
-        <div tw="flex">
-        <img src={"/images/about-us/hero/hero.jpg"} height="400" width="100%" alt = 'About VVGC'/>
-        </div>
-        
+  <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Welcome to VVGC Hindu Temple
       </div>
-    ),
+      ),
     // ImageResponse options
     {
       // For convenience, we can re-use the exported opengraph-image
