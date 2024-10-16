1aaed7efbfad9b38ac656dd844807e9cff9af195
anaghastaff <anaghastaff123@gmail.com>
2024-10-16 22:49:26 +0530

    removed twi-image.tsx and opengraph-image.tsx from events/[id] due to googlesheet api per limit per user

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,24 +2,19 @@ import { Fragment } from "react";
 import TestPage from "@modules/test/page";
 
 const Test = async ({
-  params,
   searchParams,
 }: {
-  params: {
-    camid: string;
-  };
+
   searchParams: {
     evtype: "regular" | "special" | "weekly" | "festival";
   };
 }) => {
- 
+
 
   return (
     <Fragment>
-      <TestPage 
-     
-        />
-     
+      <TestPage />
+
     </Fragment>
   );
 };
