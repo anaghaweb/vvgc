0e8185e49397eb64c9a4c58539de5e2f3a4d229d
anaghastaff <anaghastaff123@gmail.com>
2024-11-01 12:47:35 +0530

    updated maha prasadam component

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,10 @@
 declare module 'nodemailer';
+
+declare type MediaProps = {    
+    title: string;
+    url: string;
+    id: number;
+    userid?: string;
+    password?: string;
+    icon: JSX.Element;     
+};
