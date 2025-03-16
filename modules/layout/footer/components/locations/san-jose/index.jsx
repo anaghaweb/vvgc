1da161753f7c3734602b0d9b082c03788412c5d3
anaghastaff <anaghastaff123@gmail.com>
2025-03-16 19:29:35 +0530

    updated temple timing for both the temples in footer section

--- modules/layout/footer/components/locations/san-jose/index.jsx
+++ modules/layout/footer/components/locations/san-jose/index.jsx
@@ -3,19 +3,45 @@ import { IoMdMail } from "react-icons/io";
 
 const SanJose = () => {
   return (
-    <section className="text-stone-300 flex flex-col col-span-1 text-sm font-sans">
-    <p className="font-bold text-base">VVGC San Jose</p>
-    <div className="inline-flex gap-2 items-center">
-      <FaPhoneAlt /> 4082263600
-    </div>
-    <div className="inline-flex gap-2 items-center">
-      <IoMdMail /> info@vvgc.org
-    </div>
+    <section className="text-stone-300 flex flex-col text-sm font-sans gap-2">
+      <p className="font-bold text-xl">VVGC San Jose</p>
+      <div className="text-blue-300 font-bold inline-flex place-items-center gap-1">
+        <FaPhoneAlt /> <a href="tel:4082263600">4082263600</a>
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="text-blue-300 font-bold inline-flex place-items-center gap-1">
+          <IoMdMail size={20} />
+          <a href={`mailto:info@vvgc.org`}>info@vvgc.org </a>
+        </div>
+        <div className="text-blue-300 font-bold inline-flex place-items-center gap-1">
+          <IoMdMail size={20} />
+          <a href={`mailto:priests@vvgc.org`}>priests@vvgc.org </a>
+        </div>
+      </div>
+      <div>
+        <p>32, Rancho Drive</p>
+        <p>San Jose CA 95111</p>
+      </div>
+      <section className="text-stone-300 flex flex-col text-sm gap-2">
+        <p className="font-bold text-base">Temple Timings:</p>
+        <div>
+          <p className="underline">Monday - Thursday:</p>
+          <p> 10:00 AM - 12:00 noon</p>
+          <p> 6:00 PM - 8:00 PM</p>
+        </div>
+        <div>
+          <p className="underline">Friday</p>
+          <p> 8:00 AM - 10:00 AM</p>
+          <p> 5:00 PM - 8:00 PM</p>
+        </div>
+        <div>
+          <p className="underline">Saturday & Sunday</p>
+          <p> 10:00 AM - 1:00 PM</p>
+          <p> 4:00 PM - 8:00 PM </p>
+        </div>
+      </section>
+    </section>
+  );
+};
 
-    <p>32, Rancho Drive</p>
-    <p>San Jose CA 95111</p>
-  </section>
-  )
-}
-
-export default SanJose
+export default SanJose;
