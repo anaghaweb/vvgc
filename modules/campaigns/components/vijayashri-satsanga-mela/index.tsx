e8f5314bf4b24cf06abfbdc7e01079b1747cd94d
anaghastaff <anaghastaff123@gmail.com>
2025-05-08 09:02:46 +0530

    updated flyer in section one- home page

--- modules/campaigns/components/vijayashri-satsanga-mela/index.tsx
+++ modules/campaigns/components/vijayashri-satsanga-mela/index.tsx
@@ -1,78 +1,98 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
-import { FaExternalLinkAlt } from 'react-icons/fa';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import Divider from "@modules/common/components/divider";
-import { Card } from '@modules/common/components/ui/card';
+import { Card } from "@modules/common/components/ui/card";
 
 const Vijayashri_Satsanga_Mela = () => {
   const cloudinary_cdn_url = `https://res.cloudinary.com/dixkqgqsi/image/upload/v1744599039/vijayaSatsangMela.jpg`;
 
   const paypal_emailid = "Vijayashri.usa@gmail.com";
-  const paypal_link = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=${paypal_emailid}&currency_code=USD&item_name=Donation`
-
+  const paypal_link = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=${paypal_emailid}&currency_code=USD&item_name=Donation`;
 
   return (
-    <Card className="h-auto flex-1 my-4 md:my-0 mx-auto p-2 md:pl-4 col-span-1 text-left track-tight text-sm lg:text-base w-full md:w-2/3">
+    <Card className="h-auto flex-1 my-4 md:my-0 mx-auto p-2 md:pl-4 col-span-1 text-center track-tight text-sm lg:text-base w-full md:w-2/3">
       <div className="h-auto flex-1 flex flex-col gap-2 p-2 md:pl-4  track-tight text-sm lg:text-base w-full font-serif">
-        <h2 className="text-2xl md:text-3xl font-serif text-indigo-950 text-center">
-        🪔 A very happy Tamil new year to you all! 🪔
+        <h2 className="text-2xl md:text-3xl font-serif text-indigo-950 ">
+          🪔 A very happy Tamil new year to you all! 🪔
         </h2>
-        <p className=" font-semibold text-rose-600 my-2 text-center">
+        <p className=" font-semibold text-rose-600 my-2 ">
           🪔🪔Radhekrishna 🪔🪔
         </p>
         <p>
-          On the auspicious occasion of the Tamil new year , we are very happy to announce 
+          On the auspicious occasion of the Tamil new year , we are very happy
+          to announce
         </p>
         <Divider />
-        <p className="font-semibold  font-serif md:text-xl lg:text-2xl  text-rose-600 mt-4"> Vijayashri Fine Arts USA’s Satsanga Mela 2025</p>
-        <p className="mb-4 font-cormorant text-xl italic font-semibold text-center">
-          Inviting all Gurus , Shishyas , Rasikas , Bhaktas & Vijayashrians for an immersive spiritual retreat with Guru Smt Vishaka Hariji 💐
+        <p className="font-semibold  font-serif md:text-xl lg:text-2xl  text-rose-600 mt-4">
+          {" "}
+          Vijayashri Fine Arts USA’s Satsanga Mela 2025
+        </p>
+        <p className="mb-4 font-cormorant text-xl italic font-semibold ">
+          Inviting all Gurus , Shishyas , Rasikas , Bhaktas & Vijayashrians for
+          an immersive spiritual retreat with Guru Smt Vishaka Hariji 💐
         </p>
         <Divider />
         <p className=" mt-4">
-          Registration is mandatory and  please use the following link to register by May 26 2025
+          Registration is mandatory and please use the following link to
+          register by May 26 2025
         </p>
         <div className="flex justify-center w-full mb-4">
           <Link
             className={`inline-flex flex-shrink-0 justify-center items-center gap-2 max-w-fit rounded-l-full rounded-r-full   bg-blue-200 bg-opacity-75 transition-opacity hover:bg-gray-300 
                         py-2 px-4 my-1 mx-auto
                         text-xl text-indigo-800 hover:text-blue-900`}
-            target='_blank'
-            href="https://forms.gle/t627hTvAQFQ18MBy7">Register <FaExternalLinkAlt className="size-4" /></Link>
+            target="_blank"
+            href="https://forms.gle/t627hTvAQFQ18MBy7"
+          >
+            Register <FaExternalLinkAlt className="size-4" />
+          </Link>
         </div>
 
-        <p className="text-center">Please circulate the flyer and registration link to all who may be interested. </p>
+        <p className="">
+          Please circulate the flyer and registration link to all who may be
+          interested.{" "}
+        </p>
         <p>🙏 All are welcome 🙏</p>
         <Divider />
         <p>
-          We are a volunteer driven organization and donations are appreciated and welcome!
+          We are a volunteer driven organization and donations are appreciated
+          and welcome!
         </p>
-        <p className='font-opensans'>
-          Suggested donations of 51$ for a single registrant and 101$ for a family can be transferred to:
+        <p className="font-opensans">
+          Suggested donations of 51$ for a single registrant and 101$ for a
+          family can be transferred to:
         </p>
         <div className="flex justify-center w-full">
           <Link
             className={`inline-flex flex-shrink-0 justify-center items-center gap-2 max-w-fit rounded-l-full rounded-r-full   bg-blue-200 bg-opacity-75 transition-opacity hover:bg-gray-300 
                         py-2 px-4 my-1 mx-auto
                         text-xl text-indigo-800 hover:text-blue-900`}
-            target='_blank'
-            href="https://www.venmo.com/u/VijayashriUSA">Venmo <FaExternalLinkAlt className="size-4" /></Link>
+            target="_blank"
+            href="https://www.venmo.com/u/VijayashriUSA"
+          >
+            Venmo <FaExternalLinkAlt className="size-4" />
+          </Link>
         </div>
         <div className="flex justify-center w-full">
           <Link
             className={`inline-flex flex-shrink-0 justify-center items-center gap-2 max-w-fit rounded-l-full rounded-r-full   bg-blue-200 bg-opacity-75 transition-opacity hover:bg-gray-300 
                         py-2 px-4 my-1 mx-auto
                         text-xl text-indigo-800 hover:text-blue-900`}
-            target='_blank'
-            href={paypal_link}>Paypal: {paypal_emailid}<FaExternalLinkAlt className="size-4" /></Link>
+            target="_blank"
+            href={paypal_link}
+          >
+            Paypal: {paypal_emailid}
+            <FaExternalLinkAlt className="size-4" />
+          </Link>
         </div>
-
       </div>
 
       <div className="relative w-full h-[300px] md:h-[500px] lg:h-[700px] flex flex-col my-2 md:my-4 lg:my-auto p-1">
         <Link className="" href={cloudinary_cdn_url} target="_blank">
-          <Image src={cloudinary_cdn_url}
+          <Image
+            src={cloudinary_cdn_url}
             alt="event"
             sizes="(max-width: 768px) 80vw, (max-width: 1024px) 60vw, 40vw"
             className="object-contain cursor-pointer mx-auto sm:w-full"
@@ -81,7 +101,7 @@ const Vijayashri_Satsanga_Mela = () => {
         </Link>
       </div>
     </Card>
-  )
-}
+  );
+};
 
-export default Vijayashri_Satsanga_Mela
+export default Vijayashri_Satsanga_Mela;
--- modules/home/components/section-1/index.tsx
+++ modules/home/components/section-1/index.tsx
@@ -1,4 +1,3 @@
-
 import { weeklyEventsDataList } from "@lib/data/weeklyEventData";
 import DailyEventSummaryCard from "@modules/events/components/dailyEventSummaryCard";
 import SpecialEventSummaryCard from "@modules/events/components/SpecialEventSummaryCard";
@@ -9,30 +8,25 @@ import Image from "next/image";
 import Link from "next/link";
 import MahaRudraYagnaVideo from "@modules/campaigns/components/maharudra-yagnam";
 
-const SectionOne = ({ eventdata }: {
-  eventdata: CalendarEvent[]
-}) => {
+const SectionOne = ({ eventdata }: { eventdata: CalendarEvent[] }) => {
   let currentDay = getDayOfTheWeek();
   let currentDate = getCurrentDate();
   let data;
   const cdn_cloudinary_urls = [
-    `https://res.cloudinary.com/dixkqgqsi/image/upload/v1746296406/VVGC%20Events/bababalaknath20250504.jpg`
-  ]
+    `https://res.cloudinary.com/dixkqgqsi/image/upload/v1746296084/VVGC%20Events/vvgcfundraising20250511.jpg`,
+  ];
 
   try {
-    data = eventdata.find(
-      (event) => {
-        let eventDate = new Date(event.date).getDate();
-        let eventYear = new Date(event.date).getFullYear();
-        let currentYear = new Date().getFullYear();
-        if (eventDate === currentDate.getDate() && eventYear === currentYear)
-          return (eventDate === currentDate.getDate() && eventYear === currentYear)
-        else
-          return null;
-      }
-    );
+    data = eventdata.find((event) => {
+      let eventDate = new Date(event.date).getDate();
+      let eventYear = new Date(event.date).getFullYear();
+      let currentYear = new Date().getFullYear();
+      if (eventDate === currentDate.getDate() && eventYear === currentYear)
+        return eventDate === currentDate.getDate() && eventYear === currentYear;
+      else return null;
+    });
   } catch (error: any) {
-    console.log(error.name)
+    console.log(error.name);
   }
   return (
     <div className=" rounded-sm p-2">
@@ -42,12 +36,11 @@ const SectionOne = ({ eventdata }: {
             Today at the Temple, {currentDay}
           </h2>
           <Divider />
-          {
-            data?.title ?
-              <SpecialEventSummaryCard eventdata={data} />
-              :
-              <DailyEventSummaryCard events={weeklyEventsDataList} />
-          }
+          {data?.title ? (
+            <SpecialEventSummaryCard eventdata={data} />
+          ) : (
+            <DailyEventSummaryCard events={weeklyEventsDataList} />
+          )}
           {/* NEXT SPECIAL EVENT CARD */}
           <div className="leading-6">
             <Divider className="my-4" />
@@ -57,8 +50,13 @@ const SectionOne = ({ eventdata }: {
             <article className="flex flex-col gap-2">
               <Divider />
               <div className="relative w-full h-[300px] md:h-[500px] lg:h-[700px] flex flex-col my-2 md:my-4 lg:my-auto p-1">
-                <Link className="" href={cdn_cloudinary_urls[0]} target="_blank">
-                  <Image src={cdn_cloudinary_urls[0]}
+                <Link
+                  className=""
+                  href={cdn_cloudinary_urls[0]}
+                  target="_blank"
+                >
+                  <Image
+                    src={cdn_cloudinary_urls[0]}
                     alt="event"
                     sizes="(max-width: 768px) 80vw, (max-width: 1024px) 60vw, 40vw"
                     className="object-contain cursor-pointer mx-auto sm:w-full"
