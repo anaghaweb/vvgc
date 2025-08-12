5416e4437b86933ed4c323fc7006723362d1804b
anaghastaff <anaghastaff123@gmail.com>
2025-08-12 12:26:27 +0530

    added new events for august

--- modules/events/components/vishwaShantiHomam/index.tsx
+++ modules/events/components/vishwaShantiHomam/index.tsx
@@ -5,16 +5,17 @@ import { Next_Video } from "@modules/common/components/next-video-player";
 const VishwaShanthiHomamEvent = () => {
 
   const cdn_cloudinary_urls = [
-
-
-    `https://res.cloudinary.com/dixkqgqsi/image/upload/v1754189530/vvgc/images/2025/august/upakarma.jpg`,
+    `https://res.cloudinary.com/dixkqgqsi/image/upload/v1754978039/vvgc/images/2025/august/modaga_20250812.jpg`,
   ];
   const url = "https://cp30cky8vva110s1.public.blob.vercel-storage.com/varalakshmi_video.mp4";
   return (
     <div className="max-w-full mx-auto md:p-6 bg-white shadow-sm text-gray-800">
-      {/* <h1 className="text-xl md:text-2xl font-bold text-red-700 mb-4 text-center underline">
-        Yajur Upakarma
-      </h1> */}
+      <h1 className="text-xl md:text-2xl font-bold text-red-700 mb-4 text-center underline">
+        Sankata Hara Chaturthi
+      </h1>
+      <p className="text-center font-semibold font-sans text-xl">
+        12th Aug, Evening 7:30PM
+      </p>
        <div className="w-full overflow-hidden shadow h-auto my-4 mx-auto">
         <Link
           className=""
@@ -28,14 +29,7 @@ const VishwaShanthiHomamEvent = () => {
           />
         </Link>
       </div>
-       {/* <video
-        controls
-        className='mx-auto sm:w-80 md:w-96 lg:w-[400px] my-4'
-      >
-        <source src={url} type="video/mp4" />
-        Varamahalakshmi celebrations
-      </video> */}
-      {/* <Next_Video /> */}
+     
     </div>
   );
 };
