bfaec47868ef87fa25bff82a174ffef194e1357a
anaghastaff <anaghastaff123@gmail.com>
2025-10-20 21:59:03 +0530

    added skandi shasti festival card

--- modules/home/components/section-1/upcoming_event/data.ts
+++ modules/home/components/section-1/upcoming_event/data.ts
@@ -1,19 +1,5 @@
 export const upcoming_events = [
-  
-     {
-    heading: "Bhagwati Jagran",
-    subtitle: "Every 3rd friday @ Vvgc San Martin",
-    date: "October 17th",
-    time: "7:00PM",
-    imageUrl:
-      "https://res.cloudinary.com/dixkqgqsi/image/upload/v1759811931/vvgc/images/2025/october/bhagwati_jagran_oct17.jpg",
-    description: "",
-    // sponsorLinks: [
-    //   { url: "https://paybee.io/@vvgc@35", name: "Archana Sponsorship" },
-    //   { url: "https://paybee.io/@vvgc@36", name: "Pooja Sponsorship" },
-    // ],
-  }, 
-   {
+  {
     heading: "Deepavali Celebrations 2025",
     subtitle: "@ Vvgc San Martin",
     date: "October 20th & 21st",
@@ -26,7 +12,19 @@ export const upcoming_events = [
     //   { url: "https://paybee.io/@vvgc@38", name: "Cultural Program Support" },
     // ],
   },
- 
- 
- 
+  {
+    heading: "Sri Skandha Shashti Festival Celebrations",
+    subtitle: "",
+    date: "From 22nd Oct to 27th Oct",
+    time: "6:00PM",
+    imageUrl:
+      "https://res.cloudinary.com/dixkqgqsi/image/upload/v1760977179/vvgc/images/2025/october/skanda_sashti.jpg",
+    description: `Skanda Shasti marks the triumph of Lord Murugan over evil and the victory of divine virtue.
+Celebrate this sacred occasion at the temple with devotion and joy.`
+    // sponsorLinks: [
+    //   { url: "https://paybee.io/@vvgc@35", name: "Archana Sponsorship" },
+    //   { url: "https://paybee.io/@vvgc@36", name: "Pooja Sponsorship" },
+    // ],
+  },
+  
 ];
