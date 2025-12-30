470369604dfb537c54e59c4be3d96f123f8b1888
anaghastaff <anaghastaff123@gmail.com>
2025-12-30 15:23:32 +0530

    added new year events

--- modules/home/components/section-1/upcoming_event/data.ts
+++ modules/home/components/section-1/upcoming_event/data.ts
@@ -1,33 +1,37 @@
-export const upcoming_events = [  
-  
+export const upcoming_events = [
+
   {
-    heading: "Special Sri Ayyappa Mandala Pooja",
-    subtitle: "Special Sri Ayyappa Mandala Pooja Aarati And Mantra Pushpa",
-    date: ["Weekends evening at 7:00 PM"],
-    time: "",
+    heading: "Sri Vaikunta Ekadasi",
+    subtitle: "Swargal  Vaasal | Vaikunta Dwara Pooja",
+    date: [
+      "December 30th 2025",
+    ],
+    time: "Today evening from 7:30PM to 9:00PM",
     imageUrl:
-      `https://res.cloudinary.com/dixkqgqsi/image/upload/v1765174458/vvgc/images/2025/december/swamy-2.jpg`,
+      "https://res.cloudinary.com/dixkqgqsi/image/upload/v1767075777/vvgc/images/2025/december/vaikunta_ekadasi_20251230.jpg",
+    description: [`All Are Welcome to participate with family friends and Get Blessings`
+    ],
+    videoUrl: "",
+    sponsorLinks: [
+      // { url: `https://paybee.io/quickpay.html?handle=vvgc&ppid=39#optionList`, name: "Sponsor this event" },
+
+    ],
+  },
+  {
+    heading: "Mata ka Gungan",
+    subtitle: "On the eve of new year",
+    date: ["31st Dec 2025"],
+    time: "Evening 9:00PM till Midnight",
+    imageUrl:
+      ``,
     description: [`All Are Welcome To Participate With Family Friends And Get Blessings Followed By Maha Prasadam `,
     ],
+    videoUrl: "https://youtu.be/3oiauf__T_g",
     sponsorLinks: [
       // { url: "", name: "" },
     ],
   },
-    {
-    heading: "Dhanurmasa Pooja",
-    subtitle: "Daily Tiruppavai Pasuram Chanting. Watch live on Zoom, Facebook And Youtube",
-    date: ["December 16th 2025 - January 14th 2026"],
-    time: "Evening 7:00PM",
-    imageUrl:
-      `https://res.cloudinary.com/dixkqgqsi/image/upload/v1765176991/vvgc/images/2025/december/dhanurmasa-pooja-2025.jpg`,
-    description: [`Lord Krishna declares in the Bhagavad Gita: "I manifest the most in the month of Margashira" (also known as Dhanurmasam)`,
-    ],
-    sponsorLinks: [
-      // { url: "", name: "" },
-    ],
-  },
-  
-   {
+  {
     heading: "New Year Jagran",
     subtitle: "New year Jagran and Priti Bhoj on the eve of new year",
     date: ["31st Dec 2025"],
@@ -36,15 +40,89 @@ export const upcoming_events = [
       `https://res.cloudinary.com/dixkqgqsi/image/upload/v1765337295/vvgc/images/2025/december/jagrandec.jpg`,
     description: [`All Are Welcome To Participate With Family Friends And Get Blessings Followed By Maha Prasadam `,
     ],
+    videoUrl: "",
     sponsorLinks: [
       // { url: "", name: "" },
     ],
   },
+ 
+  {
+    heading: "Makara Sankranti / Pongal",
+    subtitle: "Community Pongal/Sankranti/Makara Vilakku /Makara Sankramana Celebration & Goda Kalyanam Celebration @ San Martin VVGC temple",
+    date: [
+      " Sunday - January 18th 2026",
+
+    ],
+    time: "Starts at 9:30AM",
+    imageUrl:
+      "https://res.cloudinary.com/dixkqgqsi/image/upload/v1767075851/vvgc/images/2025/december/pongal_20260118.jpg",
+    videoUrl: "",
+    description: [`All Are Welcome to participate with family friends and Get Blessings `,
+
+    ],
+    sponsorLinks: [
+      {
+        url: `https://docs.google.com/forms/d/e/1FAIpQLSe2kvH6zKxSRurJz6fddsTIm1qQOONJh3POpIhFDjGY-zui-Q/viewform`,
+        name: "Plan to attend this event? - Register Here"
+      },
+      {
+        url: `https://paybee.io/@vvgc@32`,
+        name:"Make a donation"
+      }
+
+    ],
+  },
+  {
+    heading: "Special Sri Ayyappa Mandala Pooja",
+    subtitle: "Special Sri Ayyappa Mandala Pooja Aarati And Mantra Pushpa",
+    date: ["Weekends evening at 7:00 PM"],
+    time: "",
+    imageUrl:
+      `https://res.cloudinary.com/dixkqgqsi/image/upload/v1765174458/vvgc/images/2025/december/swamy-2.jpg`,
+    videoUrl: "",
+    description: [`All Are Welcome To Participate With Family Friends And Get Blessings Followed By Maha Prasadam `,
+    ],
+    sponsorLinks: [
+      // { url: "", name: "" },
+    ],
+  },
+  {
+    heading: "Dhanurmasa Pooja",
+    subtitle: "Daily Tiruppavai Pasuram Chanting. Watch live on Zoom, Facebook And Youtube",
+    date: ["December 16th 2025 - January 14th 2026"],
+    time: "Evening 7:00PM",
+    imageUrl:
+      `https://res.cloudinary.com/dixkqgqsi/image/upload/v1765176991/vvgc/images/2025/december/dhanurmasa-pooja-2025.jpg`,
+    videoUrl: "",
+    description: [`Lord Krishna declares in the Bhagavad Gita: "I manifest the most in the month of Margashira" (also known as Dhanurmasam)`,
+    ],
+    sponsorLinks: [
+      // { url: "", name: "" },
+    ],
+  },
+
+{
+    heading: "Maha Rudra Yagnam 2026",
+    subtitle: "At this event 121 Ritwiks reciting the Sri Rudra 11 times for a total of 1331 times along with Rudra Abhishekam with 11 kinds of Dravyams. This will be followed by Rudra Homam",
+    date: ["18th April 2026"],
+    time: "From Morning 7:00AM - 1:30PM",
+    imageUrl:
+      `https://res.cloudinary.com/dixkqgqsi/image/upload/v1767075777/vvgc/images/2025/december/maha_rudra_yagnam_20260418.jpg`,
+    description: [`All Are Welcome To Participate With Family Friends And Get Blessings Followed By Maha Prasadam `,
+    ],
+    videoUrl: "",
+    sponsorLinks: [
+      { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      { url: "https://paybee.io@vvgc@73", name: "Make a donation" },
+    ],
+  },
+
+
   // {
   //   heading: "Kartika Masa Somavara Ekadasa Vara Sri Rudra Abhisheka",
   //   subtitle: "Karthika Masa is a sacred month of divine light and devotion, when every flame lit honors Lord Shiva and Lord Vishnu, purifying the soul and illuminating the path toward spiritual awakening and inner peace.",
   //   date: [
-     
+
   //     "November 3rd 2025",
   //     "November 10th 2025",
   //     "November 17th 2025",
@@ -53,11 +131,12 @@ export const upcoming_events = [
   //   time: "Every 4th Monday evening at 6.30 pm",
   //   imageUrl:
   //     "https://res.cloudinary.com/dixkqgqsi/image/upload/v1761913503/vvgc/images/2025/october/kartika_masa.jpg",
+  // videoUrl:"",
   //   description: [  `All Are Welcome to participate with family friends and Get Blessings followed by Maha rasadam`
   //   ],
   //   sponsorLinks: [
   //     { url: `https://paybee.io/quickpay.html?handle=vvgc&ppid=39#optionList`, name: "Sponsor this event" },
-      
+
   //   ],
   // },
 
