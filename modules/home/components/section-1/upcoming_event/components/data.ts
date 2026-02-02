b9cbf0d2d032dda9466bb1f74cb657ed921a0cba
anaghastaff <anaghastaff123@gmail.com>
2026-02-02 18:43:24 +0530

    added shivaratri event to home page

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -1,28 +1,57 @@
 export const upcoming_events = [  
- 
+  //SHIVARATRI
   {
-    heading: "Thai Poosam Kavadi Festival Celebrations",
-    subtitle: "",
-    date: ["1st Feb 2026"],
-    time: "Evening 7:30PM onwards",
-    imageUrl:`https://res.cloudinary.com/dixkqgqsi/image/upload/v1768454126/vvgc/images/2026/feb/thai_poosam_Festival.jpg`,
+    heading: "Maha Shivaratri Mahotsavam",
+    subtitle: "Special Event Shiva Parvati (Girija) Kalyanam",
+    date: ["15th Febraury"],
+    time: "From Morning 10:00AM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1770013411/vvgc/images/2026/feb/mahashivaratri_1.jpg",
+        alt:"shivratriimage1",
+        id:1
+    },
+     
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1768454126/vvgc/images/2026/feb/shivaratri_2026.jpg",
+        alt:"shivaratriimage3",
+        id:3
+    },
+    {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1770013412/vvgc/images/2026/feb/mahashivaratri_weekend_event.jpg",
+        alt:"shivaratriimage2",
+        id:2
+    },
+  ],
     description: [`All Are Welcome To Participate With Family Friends And Get Blessings Followed By Maha Prasadam `,
     ],
     videoUrl: "",
     details:[],
     sponsorLinks: [
-        {
-        url: `https://paybee.io/@vvgc@32`,
-        name: "Sponsor this event"
-      },
+      //   {
+      //   url: `https://vvgc.org/events/event_661`,
+      //   name: "Click here for complete details"
+      // },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
     ],
   },
+
+  //MAHA RUDRA YAGNAM
+  
   {
     heading: "Maha Rudra Yagnam 2026",
     subtitle: "At this event 121 Ritwiks reciting the Sri Rudra 11 times for a total of 1331 times along with Rudra Abhishekam with 11 kinds of Dravyams. This will be followed by Rudra Homam",
     date: ["18th April 2026"],
     time: "From Morning 7:00AM - 1:30PM",
-    imageUrl:``,
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1767585425/vvgc/images/2026/jan/maha_rudra_yagnam_20260418.jpg",
+        alt:"shivratriimage1",
+        id:1
+    },
+    
+  ],
     description: [`All Are Welcome To Participate With Family Friends And Get Blessings Followed By Maha Prasadam `,
     ],
     videoUrl: "",
@@ -36,5 +65,6 @@ export const upcoming_events = [
       { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
     ],
   },
+  
 ];
 
