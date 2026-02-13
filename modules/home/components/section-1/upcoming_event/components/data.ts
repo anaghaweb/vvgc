28469e1c9160d48a591cda42363a1628f60985cd
anaghastaff <anaghastaff123@gmail.com>
2026-02-13 12:12:00 +0530

    updated special funding flyer in donations page and added bhagwati jagran flyer in home page

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -37,7 +37,33 @@ export const upcoming_events = [
     ],
   },
 
-  //MAHA RUDRA YAGNAM
+  //BHAGWATI JAGRAN
+  {
+    heading: "Bhagwati Jagran",
+    subtitle: "Priti Bhoj & Mata ki Chowki",
+    date: ["Fri 20th Feb 2026"],
+    time: "Evening 7:00 PM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1770963675/vvgc/images/2026/feb/bhagwati_jagran_20260220.jpg",
+        alt:"bhagwati_jagran_image",
+        id:1
+    },
+    
+  ],
+    description: [`All Are Welcome To Participate With Family Friends And Get Blessings Followed By Maha Prasadam `,
+    ],
+    videoUrl: "",
+    details:[],
+    sponsorLinks: [
+      //   {
+      //   url: `https://vvgc.org/events/event_661`,
+      //   name: "Click here for complete details"
+      // },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
   
   {
     heading: "Maha Rudra Yagnam 2026",
