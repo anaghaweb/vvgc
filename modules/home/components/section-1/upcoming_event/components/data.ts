d9bc3c10b9f87975c95b940fc815b82098154230
anaghastaff <anaghastaff123@gmail.com>
2026-03-17 18:19:17 +0530

    added new events for vasantha navaratri celebrations

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -1,5 +1,65 @@
 export const upcoming_events = [  
   
+  // CHANDI HOMAM
+  {
+    heading: "🔥 Chandi Homam 🔥",
+    subtitle: "Vasantha Navarathri Special at VVGC",
+    date: ["Saturday, March 21"],
+    time: "11:00 AM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1773645305/vvgc/images/2026/march/chandi_homam_0321.jpg",
+        alt:"An Image of chandi homam 2026",
+        id:1
+    },
+    
+  ],
+    description: [`As part of the Vasantha Navarathri celebrations, devotees are invited to participate in the powerful Chandi Homam.`,
+    ],
+    videoUrl: "",
+    details:["This sacred homam invokes the blessings of Divine Mother Durga for protection, prosperity, and spiritual strength. Maha Prasadam & Preethi Bhoj will be served",
+      " Participate with your family and receive the blessings of the Divine Mother"
+    ],
+    sponsorLinks: [
+        {
+        url: `https://paybee.io/@vvgc@34`,
+        name: "For registration & sponsorship:"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
+  // SUVASINI POOJA
+  {
+    heading: "🌺 Suvasini Pooja 🌺",
+    subtitle: "Vasantha Navarathri Special at VVGC",
+    date: ["Sunday, March 22"],
+    time: "11:00 AM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1773645305/vvgc/images/2026/march/suvasini_pooja_0322.jpg",
+        alt:"An Image of chandi homam 2026",
+        id:1
+    },
+    
+  ],
+    description: [`Families are warmly invited to take part in this sacred celebration and receive the blessings of the Divine Mother`,
+    ],
+    videoUrl: "",
+    details:["Each Suvasini will perform Deepam & Kumkum Archana, offering prayers to Goddess Lalitha for family wellbeing, prosperity, and peace. Maha Prasadam & Preethi Bhoj will be served",
+      "Register today to be part of the Suvasini Pooja and support our Navarathri celebrations with your kind sponsorship."
+
+    ],
+    sponsorLinks: [
+        {
+        url: `https://paybee.io/@vvgc@33`,
+        name: "For registration & sponsorship:"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
+  
     //BHAGWATI JAGRAN
    {
     heading: "Bhagwati Jagran",
@@ -27,6 +87,35 @@ export const upcoming_events = [
       // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
     ],
   },
+  // ANANDA DANA
+
+   {
+    heading: "Ananda Dana",
+    subtitle: "A fundraiser for Anandam, India",
+    date: ["11th April 2026"],
+    time: "3:30PM - 6:00PM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1773751033/vvgc/images/2026/april/ananda_dana_0411.jpg",
+        alt:"ananda danam image",
+        id:1
+    },
+    
+  ],
+    description: [`All Are Welcome To Participate With Family Friends And a musical evening `,
+    ],
+    videoUrl: "",
+    details:[],
+    sponsorLinks: [
+        {
+        url: `https://scan.page/7b00ZP`,
+        name: "To attend please register"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
+  // MAHA RUDRA YAGNAM
   {
     heading: "Maha Rudra Yagnam 2026",
     subtitle: "At this event 121 Ritwiks reciting the Sri Rudra 11 times for a total of 1331 times along with Rudra Abhishekam with 11 kinds of Dravyams. This will be followed by Rudra Homam",
@@ -53,6 +142,7 @@ export const upcoming_events = [
       { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
     ],
   },
+  
   
 ];
 
