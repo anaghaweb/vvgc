6f9c724f68b555197e79c24a5a4ae3fde79901fe
anaghastaff <anaghastaff123@gmail.com>
2026-04-07 18:25:46 +0530

    added new events

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -1,11 +1,5 @@
 export const upcoming_events = [  
   
-  // VASANTHA PANCHAMI
-  
-  //RAMA NAVAMI CELEBRATIONS
-
-  
-  
   // ANANDA DANA
 
    {
@@ -66,5 +60,111 @@ export const upcoming_events = [
       { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
     ],
   },  
+  //VVGC DEVELOPMENT FUNDRAISER
+   {
+    heading: "Chitra Pournami Pooja",
+    subtitle: "Temple Development Fundraiser Event",
+    date: ["1st & 2nd May"],
+    time: "1st May - 7:00PM, 2nd May - 11:00AM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1775543914/vvgc/images/2026/may/fundraiser_0501_02.jpg",
+        alt:"fundraiser image",
+        id:1
+    },    
+  ],
+    description: [`All Are Welcome To Participate With Family and Friends `,
+    ],
+    videoUrl: "",
+    details:[],
+    sponsorLinks: [
+        {
+        url: `https://paybee.io/quickpay.html?handle=vvgc&ppid=9`,
+        name: "click here to donate"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
+
+  // Sri Meenakshi Sundareswarar Kalyanam Utsavam
+  {
+    heading: "Sri Meenakshi Sundareswarar Kalyanam Utsavam",
+    subtitle: "",
+    date: ["2nd May"],
+    time: "12:30PM - 2:00PM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1775543920/vvgc/images/2026/may/meenakshi_sundareshwar_thirukalyana_utsavam_0502.jpg",
+        alt:"meenakshi sundareshwar utsavam",
+        id:1
+    },    
+  ],
+    description: [`All Are Welcome To Participate With Family and Friends`,
+    ],
+    videoUrl: "",
+    details:[],
+    sponsorLinks: [
+        {
+        url: `https://paybee.io/quickpay.html?handle=vvgc&ppid=52#campaignList`,
+        name: "Sponsor this event"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
+  //HANUMAN JAYANTHI
+  {
+    heading: "Chandramana Sri Hanuman Jayanthi Celebrations",
+    subtitle: "",
+    date: ["12th May"],
+    time: "7:00PM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1775543913/vvgc/images/2026/may/chandramana_hanuman_jayanthi_0512.jpg",
+        alt:"hanuman jayanthi",
+        id:1
+    },    
+  ],
+    description: [`All Are Welcome To Participate With Family and Friends`,
+    ],
+    videoUrl: "",
+    details:[],
+    sponsorLinks: [
+        {
+        url: `https://paybee.io/quickpay.html?handle=vvgc&ppid=52#optionList`,
+        name: "Click here to donate"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
+
+  //23RD YEAR ANNIVERSARY
+  {
+    heading: "VVGC 23rd Year Anniversary Celebrations",
+    subtitle: "",
+    date: ["6th June"],
+    time: "Starts at 9:00AM",
+    imageArray:[
+     {
+        url:"https://res.cloudinary.com/dixkqgqsi/image/upload/v1775543977/vvgc/images/2026/june/vvgc_23rd_anniversary.jpg",
+        alt:"VVGC 23rd Year Anniversary Celebrations",
+        id:1
+    },    
+  ],
+    description: [`All Are Welcome To Participate With Family and Friends`,
+    ],
+    videoUrl: "",
+    details:[],
+    sponsorLinks: [
+        {
+        url: `https://paybee.io/@vvgc@64`,
+        name: "Click here to donate"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
 ];
 
