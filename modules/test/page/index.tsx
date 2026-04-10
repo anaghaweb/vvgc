549437284dabb5d780fcbc866dc873f2bb73bace
float-bsk <projektsbsk@gmail.com>
2026-04-10 12:36:23 +0530

    removed deleted imports and components

--- modules/test/page/index.tsx
+++ modules/test/page/index.tsx
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
-import Events from "../components/events"; 
+
 import MahaRudraYagnamSummaryLink from "@modules/home/components/section-1/maha-rudra-yagnam-2026/components/summary-link";
 import CalenderWithTimeZone from "../calendar";
 const TestPage = async () => {
     const response = false;
   return (
     <Fragment>
-      {/* <Events /> */}
+    
       <MahaRudraYagnamSummaryLink />
       <CalenderWithTimeZone />
     </Fragment>
