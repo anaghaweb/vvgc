f7d670bffb9d837b821474d410c35ee7ef8c59a6
anaghastaff <anaghastaff123@gmail.com>
2026-04-13 14:39:06 +0530

    updated gtm scripts in analytics/index.tsx

--- modules/analytics/index.tsx
+++ modules/analytics/index.tsx
@@ -5,12 +5,13 @@ const gtm_script = `
  window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', ${gtmid});
 `
 export default function GoogleTagManager() {
         return <>
-                <Script src={`https://www.googletagmanager.com/gtag/js?id=${gtmid}`} strategy="afterInteractive"/>
-                <Script src={gtm_script} strategy="afterInteractive"/>              
+                <Script async src={`https://www.googletagmanager.com/gtag/js?id=${gtmid}`} strategy="afterInteractive"/>
+                <Script  strategy="afterInteractive"> 
+                {gtm_script}
+                </Script>
         </>
 }
