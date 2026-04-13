65646666e1b928dc1cbd934afc752811eea974f3
anaghastaff <anaghastaff123@gmail.com>
2026-04-13 14:27:39 +0530

    testing google analytics. updated gta scripts

--- modules/analytics/index.tsx
+++ modules/analytics/index.tsx
@@ -2,23 +2,15 @@ import Script from "next/script";
 
 const gtmid = process.env.GTM_ID;
 const gtm_script = `
- (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer',${gtmid});
+ window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ${gtmid});
 `
 export default function GoogleTagManager() {
         return <>
-                <Script>
-                        {gtm_script}
-                </Script>
-                {/* <!-- Google Tag Manager (noscript) --> */}
-                <noscript>
-                <iframe src={`https://www.googletagmanager.com/ns.html?id=${gtmid}`}
-                        height="0" width="0" style={{ display: "none", visibility: "hidden" }}>
-                </iframe>
-                </noscript>
-                {/* <!-- End Google Tag Manager (noscript) --> */}
+                <Script src={`https://www.googletagmanager.com/gtag/js?id=${gtmid}`} strategy="afterInteractive"/>
+                <Script src={gtm_script} strategy="afterInteractive"/>              
         </>
 }
