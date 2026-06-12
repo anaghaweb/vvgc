f80a412582174a1b76e4308bfcd3a5c67708a539
anaghastaff <anaghastaff123@gmail.com>
2026-06-12 18:29:32 +0530

    added new event 19th june

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -26,6 +26,32 @@ export const upcoming_events = [
       // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
     ],
   },
+   //Bhagwati Jagran 19TH JUNE
+  {
+    heading: "Bhagwati Jagran ",
+    subtitle: "Followed by Priti Bhoj",
+    date: ["19th June"],
+    time: "7:00 PM",
+    imageArray: [
+      {
+        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1781268328/vvgc/images/2026/june/bhagwati_jagran_0619.jpg",
+        alt: "bhagwati jagran Image",
+        id: 1
+      },
+    ],
+    description: [`All Are Welcome To Participate With Family and Friends`,
+    ],
+    videoUrl: "", 
+    details: [],
+    sponsorLinks: [
+      // {
+      //   url: `https://paybee.io/@vvgc@64`,
+      //   name: "Click here to donate"
+      // },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
 
 ];
 
