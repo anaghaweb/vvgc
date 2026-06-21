50e281e7b35fc02dff51c23249a8a22bbbbcca7b
anaghastaff <anaghastaff123@gmail.com>
2026-06-21 19:35:17 +0530

    added maha rudram event on 18th & 19th july 2026

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -2,14 +2,14 @@ export const upcoming_events = [
  
     //KAMBARAMAYANAM 13TH JUNE
   {
-    heading: "KambaRamayanam",
-    subtitle: "By Dr. Priya Ramachandran",
-    date: ["13th June"],
-    time: "11:00 am to 12:00 noon",
+    heading: "Maha Rudram",
+    subtitle: "Program held at VVGC San Martin",
+    date: ["18th & 19th July"],
+    time: "6:30 PM on 18th July",
     imageArray: [
       {
-        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1779170813/vvgc/images/2026/june/kambaramayanam_0611.webp",
-        alt: "Kambaramayanam",
+        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1782049134/vvgc/images/2026/july/maha_rudram_0718.jpg",
+        alt: "Maha Rudram",
         id: 1
       },
     ],
@@ -18,40 +18,39 @@ export const upcoming_events = [
     videoUrl: "", 
     details: [],
     sponsorLinks: [
-      // {
-      //   url: `https://paybee.io/@vvgc@64`,
-      //   name: "Click here to donate"
-      // },
-      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
-      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+      {
+        url: `https://forms.gle/ZmgtHvz9FF96er2w6`,
+        name: "Register for Suvasini event"
+      },
+      { url: "https://forms.gle/mRkVwQKgFN23Q9TG8", name: "Register for Ritwik Event" },
     ],
   },
    //Bhagwati Jagran 19TH JUNE
-  {
-    heading: "Bhagwati Jagran ",
-    subtitle: "Followed by Priti Bhoj",
-    date: ["19th June"],
-    time: "7:00 PM",
-    imageArray: [
-      {
-        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1781268328/vvgc/images/2026/june/bhagwati_jagran_0619.jpg",
-        alt: "bhagwati jagran Image",
-        id: 1
-      },
-    ],
-    description: [`All Are Welcome To Participate With Family and Friends`,
-    ],
-    videoUrl: "", 
-    details: [],
-    sponsorLinks: [
-      // {
-      //   url: `https://paybee.io/@vvgc@64`,
-      //   name: "Click here to donate"
-      // },
-      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
-      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
-    ],
-  },
+  // {
+  //   heading: "Bhagwati Jagran ",
+  //   subtitle: "Followed by Priti Bhoj",
+  //   date: ["19th June"],
+  //   time: "7:00 PM",
+  //   imageArray: [
+  //     {
+  //       url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1781268328/vvgc/images/2026/june/bhagwati_jagran_0619.jpg",
+  //       alt: "bhagwati jagran Image",
+  //       id: 1
+  //     },
+  //   ],
+  //   description: [`All Are Welcome To Participate With Family and Friends`,
+  //   ],
+  //   videoUrl: "", 
+  //   details: [],
+  //   sponsorLinks: [
+  //     // {
+  //     //   url: `https://paybee.io/@vvgc@64`,
+  //     //   name: "Click here to donate"
+  //     // },
+  //     // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+  //     // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+  //   ],
+  // },
 
 ];
 
