4e6ee2a4d8390a8c0981700a984e083f9db07e25
anaghastaff <anaghastaff123@gmail.com>
2026-07-24 08:45:07 +0530

    removed past events

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -1,30 +1,7 @@
 export const upcoming_events = [
  
     //Maharudram 18th,19th July
-  {
-    heading: "Maha Rudram",
-    subtitle: "Program held at VVGC San Martin",
-    date: ["18th & 19th July"],
-    time: "6:30 PM on 18th July",
-    imageArray: [
-      {
-        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1782049134/vvgc/images/2026/july/maha_rudram_0718.jpg",
-        alt: "Maha Rudram",
-        id: 1
-      },
-    ],
-    description: [`All Are Welcome To Participate With Family and Friends`,
-    ],
-    videoUrl: "", 
-    details: [],
-    sponsorLinks: [
-      {
-        url: `https://forms.gle/ZmgtHvz9FF96er2w6`,
-        name: "Register for Suvasini event"
-      },
-      { url: "https://forms.gle/mRkVwQKgFN23Q9TG8", name: "Register for Ritwik Event" },
-    ],
-  },
+  
   //  Bhagwati Jagran 19TH JUNE
   {
     heading: "Vitthal Wari Celebration",
