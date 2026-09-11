0c803f11de038b250fca7db6de833332d95e1515
anaghastaff <anaghastaff123@gmail.com>
2026-09-11 10:18:58 +0530

    updated home page events

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -1,30 +1,5 @@
 export const upcoming_events = [
-    //  Shirdi Sai Aarthi
-  {
-    heading: "Shri Shirdi Sai Aarthi",
-    subtitle: "Every thursday evening @ VVGC San Martin",
-    date: ["Spetember 3rd, 10th, 17th & 24th"],
-    time: "10 am, 1 pm, 6 pm, 8 pm",
-    imageArray: [
-      {
-        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1788932527/vvgc/images/2026/recurring/shirdisai_aarathi.jpg",
-        alt: "Vitthal Wari Flyer",
-        id: 1
-      },
-    ],
-    description: [`All Are Welcome To Participate With Family and Friends`,
-    ],
-    videoUrl: "", 
-    details: [],
-    sponsorLinks: [
-      {
-        url: `https://paybee.io/@vvgc@32`,
-        name: "Contribute to this event"
-      },
-      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
-      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
-    ],
-  },
+
    //SANGAMAM SEPT 12TH 2026
    {
     heading: "Sangamam",
@@ -222,6 +197,32 @@ export const upcoming_events = [
       // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
     ],
   },  
+      //  Shirdi Sai Aarthi
+  {
+    heading: "Shri Shirdi Sai Aarthi",
+    subtitle: "Every thursday evening @ VVGC San Martin",
+    date: ["Spetember 17th & 24th"],
+    time: "10 am, 1 pm, 6 pm, 8 pm",
+    imageArray: [
+      {
+        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1788932527/vvgc/images/2026/recurring/shirdisai_aarathi.jpg",
+        alt: "Vitthal Wari Flyer",
+        id: 1
+      },
+    ],
+    description: [`All Are Welcome To Participate With Family and Friends`,
+    ],
+    videoUrl: "", 
+    details: [],
+    sponsorLinks: [
+      {
+        url: `https://paybee.io/@vvgc@32`,
+        name: "Contribute to this event"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  },
 ];
 
 //  BHAGWATI JAGRAN 21ST AUG
