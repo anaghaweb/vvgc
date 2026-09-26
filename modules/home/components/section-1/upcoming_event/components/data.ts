71b0fbb8b155d6f25465cb19f12d2843b432361b
anaghastaff <anaghastaff123@gmail.com>
2026-09-26 06:21:34 +0530

    added new events sept, oct

--- modules/home/components/section-1/upcoming_event/components/data.ts
+++ modules/home/components/section-1/upcoming_event/components/data.ts
@@ -28,14 +28,40 @@ export const upcoming_events = [
     ],
   },
    //Shirdi Sai Aarthi
+  // {
+  //   heading: "Shri Shirdi Sai Aarthi",
+  //   subtitle: "Every thursday evening @ VVGC San Martin",
+  //   date: ["Spetember 24th"],
+  //   time: "10 am, 1 pm, 6 pm, 8 pm",
+  //   imageArray: [
+  //     {
+  //       url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1788932527/vvgc/images/2026/recurring/shirdisai_aarathi.jpg",
+  //       alt: "Vitthal Wari Flyer",
+  //       id: 1
+  //     },
+  //   ],
+  //   description: [`All Are Welcome To Participate With Family and Friends`,
+  //   ],
+  //   videoUrl: "", 
+  //   details: [],
+  //   sponsorLinks: [
+  //     {
+  //       url: `https://paybee.io/@vvgc@32`,
+  //       name: "Contribute to this event"
+  //     },
+  //     // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+  //     // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+  //   ],
+  // }, 
+  //Sundarkand Path
   {
-    heading: "Shri Shirdi Sai Aarthi",
-    subtitle: "Every thursday evening @ VVGC San Martin",
-    date: ["Spetember 24th"],
-    time: "10 am, 1 pm, 6 pm, 8 pm",
+    heading: "Sundarkand Path",
+    subtitle: "",
+    date: ["Spetember 26th"],
+    time: "7:30 PM",
     imageArray: [
       {
-        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1788932527/vvgc/images/2026/recurring/shirdisai_aarathi.jpg",
+        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1790381142/vvgc/images/2026/sept/sundarkand_path.jpg",
         alt: "Vitthal Wari Flyer",
         id: 1
       },
@@ -47,6 +73,33 @@ export const upcoming_events = [
     sponsorLinks: [
       {
         url: `https://paybee.io/@vvgc@32`,
+        name: "Contribute to this event"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  }, 
+
+    //Weekly Hanuman Chalisa
+  {
+    heading: "Weekly Hanuman Chalisa",
+    subtitle: "Every Saturday",
+    date: ["Spetember 26th"],
+    time: "6:30 PM",
+    imageArray: [
+      {
+        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1790381574/vvgc/images/2026/recurring/weekly_hanuman_chalisa_sat.jpg",
+        alt: "weekly hanuman chalisa",
+        id: 1
+      },
+    ],
+    description: [`All Are Welcome To Participate With Family and Friends`,
+    ],
+    videoUrl: "", 
+    details: [],
+    sponsorLinks: [
+      {
+        url: `https://paybee.io/@vvgc@014`,
         name: "Contribute to this event"
       },
       // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
@@ -105,6 +158,58 @@ export const upcoming_events = [
       // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
     ],
   },   
+    //Chandi Homam
+  {
+    heading: "Sri Chandi Homam",
+    subtitle: "Sharadha Navarathri Celebrations",
+    date: ["October 17th"],
+    time: "Homam starts @ 11:00 AM",
+    imageArray: [
+      {
+        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1790381379/vvgc/images/2026/oct/sharadha_navarathri_chandihomam.jpg",
+        alt: "Vitthal Wari Flyer",
+        id: 1
+      },
+    ],
+    description: [`All Are Welcome To Participate With Family and Friends`,
+    ],
+    videoUrl: "", 
+    details: [],
+    sponsorLinks: [
+      {
+        url: `https://paybee.io/@vvgc@34`,
+        name: "Contribute to this event"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  }, 
+     //Suvasini Pooja
+  {
+    heading: "Suvasini Pooja & Chenda Melam",
+    subtitle: "Sharadha Navarathri Celebrations",
+    date: ["October 17th"],
+    time: "Pooja starts @ 11:00 AM",
+    imageArray: [
+      {
+        url: "https://res.cloudinary.com/dixkqgqsi/image/upload/v1790381403/vvgc/images/2026/oct/sharadha_navarathri_suvasini_pooja.jpg",
+        alt: "suvasini pooja image",
+        id: 1
+      },
+    ],
+    description: [`All Are Welcome To Participate With Family and Friends`,
+    ],
+    videoUrl: "", 
+    details: [],
+    sponsorLinks: [
+      {
+        url: `https://paybee.io/@vvgc@33`,
+        name: "Contribute to this event"
+      },
+      // { url: "https://bit.ly/vvgc-maharudram", name: "Register Here" },
+      // { url: "https://paybee.io/@vvgc@73", name: "Make a donation" },
+    ],
+  }, 
 ];
 
 //BHAGWATI JAGRAN 21ST AUG
